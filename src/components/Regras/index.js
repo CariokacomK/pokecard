a1d3ea1b140d6216ee9abcbdadb3ac1e2f4373b5
Gabriel Borges <gobcarioka@gmail.com>
2024-11-06 20:42:53 -0300

    feat add lógica booleana

--- src/components/Regras/index.js
+++ src/components/Regras/index.js
@@ -3,25 +3,21 @@ import './Regras.css'
 
 const Regras = ({ isOpen, setModalOpen }) => {
 
-    if (isOpen) {
-        return (
-            <div className='content-rules'>
-                <div className='content-close'>
-                    <CloseOutlined style={{cursor:'pointer'}} onClick={setModalOpen}/>
-                </div>
-                <h1>Olá, Bem vindo ao PokeCard!</h1>
-                <h2>Regras</h2>
-                <p>Bem o jogo funciona como uma batalha por atríbutos, escolha um atributo de sua pokecard e caso o valor seja maior ou igual que de seu oponente você ganha</p>
-                <ol>
-                    <li>1 - Quem ganhar 10 batalhas vence</li>
-                    <li>2 - A batalha entre elementos é definida pela imagem abaixo</li>
-                </ol>
+    return (
+
+        isOpen && (<div className='content-rules'>
+            <div className='content-close'>
+                <CloseOutlined style={{ cursor: 'pointer' }} onClick={setModalOpen} />
             </div>
-        )
-    }
-
-    return null;
-
+            <h1>Olá, Bem vindo ao PokeCard!</h1>
+            <h2>Regras</h2>
+            <p>Bem o jogo funciona como uma batalha por atríbutos, escolha um atributo de sua pokecard e caso o valor seja maior ou igual que de seu oponente você ganha</p>
+            <ol>
+                <li>1 - Quem ganhar 10 batalhas vence</li>
+                <li>2 - A batalha entre elementos é definida pela imagem abaixo</li>
+            </ol>
+        </div>)
+    )
 
 }
 
