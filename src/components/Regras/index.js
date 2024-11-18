3aa4e6e30b62e6c0717fbd30740d87c093a74fde
Gabriel Borges <gabriel.borges@happy.com.br>
2024-11-18 17:33:03 -0300

    add react-router-dom

--- src/components/Regras/index.js
+++ src/components/Regras/index.js
@@ -11,7 +11,7 @@ const Regras = ({ isOpen, closeModal }) => {
             </div>
             <h1>Olá, Bem vindo ao PokeCard!</h1>
             <h2>Regras</h2>
-            <p>Bem o jogo funciona como uma batalha por atríbutos, escolha um atributo de sua pokecard e caso o valor seja maior ou igual que de seu oponente você ganha</p>
+            <p>Bem o jogo funciona como uma batalha por atríbutos, escolha um atributo de sua pokecard e caso o valor seja maior ou igual que de seu oponente você ganha a batalha</p>
             <ol>
                 <li>1 - Quem ganhar 10 batalhas vence</li>
                 <li>2 - A batalha entre elementos é definida pela imagem abaixo</li>
